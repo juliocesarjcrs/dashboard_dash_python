182025e0e5e315aebc23ad092b774404ea1cd669
Julio Rico <Julio.Rico@bitsamericas.com>
2022-06-04 17:04:12 -0500

    prueba warning

--- lib/co_map.py
+++ lib/co_map.py
@@ -8,11 +8,15 @@ import plotly.express as px
 import pandas as pd
 import os
 
-DATA_DIR = "data"
-cities_path = os.path.join(DATA_DIR, "worldcities.csv")
-world_cities = pd.read_csv(cities_path)
-cities = world_cities[world_cities['iso2'] == 'CO']
-cities['magnitude'] = np.random.randint(1, 300, cities.shape[0])
+# DATA_DIR = "data"
+# cities_path = os.path.join(DATA_DIR, "worldcities.csv")
+# world_cities = pd.read_csv(cities_path)
+# cities = world_cities[world_cities['iso2'] == 'CO']
+# cities['magnitude'] = np.random.randint(1, 300, cities.shape[0])
+cities = pd.DataFrame()
+
+d = {'lat': [1, 2], 'lng': [3, 4], 'magnitude': [1, 0]}
+cities = pd.DataFrame(data=d)
 
 Map_Fig = px.density_mapbox(cities, lat='lat', lon='lng', z='magnitude', radius=10,
                         center=dict(lat=4.570868, lon=-74.297333),
--- pages/analysis.py
+++ pages/analysis.py
@@ -2,9 +2,9 @@ from dash import html
 from lib import co_map
 page = html.P("This is the content of the analysis page!")
 
-page = html.Div(
-    [
-        co_map.map,
-    ],
-    className="ds4a-app",  # You can also add your own css files by storing them in the assets folder
-)
+# page = html.Div(
+#     [
+#         co_map.map,
+#     ],
+#     className="ds4a-app",  # You can also add your own css files by storing them in the assets folder
+# )
