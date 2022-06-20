8b90a9e14733be8d8fd605b418057c1c85905080
masl512 <miguel.soler@mail.escuelaing.edu.co>
2022-06-20 09:52:21 -0500

    modif Miguel 20 Jun

--- lib/co_map.py
+++ lib/co_map.py
@@ -8,11 +8,12 @@ import plotly.express as px
 import pandas as pd
 import os
 
-# DATA_DIR = "data"
+DATA_DIR = "data"
 # cities_path = os.path.join(DATA_DIR, "worldcities.csv")
-# world_cities = pd.read_csv(cities_path)
-# cities = world_cities[world_cities['iso2'] == 'CO']
-# cities['magnitude'] = np.random.randint(1, 300, cities.shape[0])
+cities_path = 'dashboard_dash_python\data\worldcities.csv'
+world_cities = pd.read_csv(cities_path)
+cities = world_cities[world_cities['iso2'] == 'CO']
+cities['magnitude'] = np.random.randint(1, 300, cities.shape[0])
 cities = pd.DataFrame()
 
 d = {'lat': [1, 2], 'lng': [3, 4], 'magnitude': [1, 0]}
@@ -21,8 +22,8 @@ cities = pd.DataFrame(data=d)
 Map_Fig = px.density_mapbox(cities, lat='lat', lon='lng', z='magnitude', radius=10,
                         center=dict(lat=4.570868, lon=-74.297333),
                         zoom=3, #zoom (int (default 8)) – Between 0 and 20. Sets map zoom level.
-                        mapbox_style="stamen-terrain")
-Map_Fig.update_layout(title="COL map", paper_bgcolor="#F8F9F9")
+                        )
+Map_Fig.update_layout(title="COL map",geo_scope = "south america", paper_bgcolor="#F8F9F9")
 
 
 ##############################
