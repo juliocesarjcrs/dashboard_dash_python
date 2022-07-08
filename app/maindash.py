ded77ede95d1a3a57eacf0ccb14acb6f1b186cc2
Julio Cesar <jcrico27@misena.edu.co>
2022-07-07 23:35:06 -0500

    authentication

--- app/maindash.py
+++ app/maindash.py
@@ -1,3 +1,17 @@
 ## MAIN APP INSTANCE ##
+import dash_auth
 from dash import Dash
+
+from decouple import config
+# Get environment variables
+USER_APP = config('USER_APP')
+PASSWORD_APP = config('PASSWORD_APP')
+
+VALID_USERNAME_PASSWORD_PAIRS = {
+    USER_APP: PASSWORD_APP
+}
 app = Dash(__name__, suppress_callback_exceptions=True)
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS
+)
